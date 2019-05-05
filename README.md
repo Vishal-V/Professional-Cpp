e17f1c73768e278fcbfb374672892e65a5495eb7
Vishal-V <vishal114186@gmail.com>
2019-05-05 19:42:55 +0530

    Revision material

---
+++ README.md
@@ -0,0 +1,2 @@
+# Professional C++
+Professional C++17 code based on software practices and new methodologies from expert programmers. 
---
+++ revision.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <initializer_list>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <memory>
+using namespace std;
+
+int addition(initializer_list<int> lst){
+    int summed = 0;
+    for(int val : lst){
+        summed += val;
+    }
+    return summed;
+}
+
+bool cmp(int a, int b){
+    return a > b;
+}
+
+void func(char* str){
+    cout << "char* version" << endl;
+}
+
+void func(int i) {
+    cout << "int version" << endl;
+}
+
+int main(){
+    vector<int> v = {3,2,1};
+    v.push_back(0);
+    sort(v.begin(), v.end(), cmp);
+    for(int val : v){
+        cout << val << endl;
+    }
+    int * arr = new int[3];
+    for(int i=0;i<3;i++)
+        arr[i] = i;
+    cout << addition({1,2,3}) << endl;
+
+    typedef struct {
+        string name;
+        int id;
+        long salary;
+    }Employee;
+
+    auto emps = make_unique<Employee[]>(10); 
+    unique_ptr<Employee> emp1(new Employee);   
+
+    delete[] arr;
+    arr = nullptr;
+
+}
